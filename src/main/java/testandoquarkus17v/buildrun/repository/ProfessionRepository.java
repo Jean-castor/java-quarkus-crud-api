09d9ca48ed3815a0e2223b36ad33d003b4a4ec5c
Jean-castor <128548264+Jean-castor@users.noreply.github.com>
2025-06-18 12:01:42 -0300

    Update ProfessionRepository.java

--- src/main/java/testandoquarkus17v/buildrun/repository/ProfessionRepository.java
+++ src/main/java/testandoquarkus17v/buildrun/repository/ProfessionRepository.java
@@ -9,7 +9,3 @@ import java.util.UUID;
 @ApplicationScoped
 public class ProfessionRepository implements PanacheRepositoryBase<ProfessionEntity, UUID> {
 }
-
-// ESTABELECE A RELAÇÃO COM O BANCO DE DADOS → PanacheRepository
-// PanacheRepository ENVIA A ENTIDADE PARA O BANCO DE DADOS
-// ProfessionEntity DEFINE NO BANCO (nome da tabela) (nome das colunas)
